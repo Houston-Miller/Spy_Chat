cda8bc1f3ae409512f7843c9df976e74e697e5cc
Houston Miller <houston.miller93@gmail.com>
2026-03-08 13:32:22 -0400

    initial commit

---
+++ ReactClient/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { HubConnectionBuilder } from '@microsoft/signalr';
+
+const connection = new HubConnectionBuilder()
+    .withUrl('http://localhost:5062')
+    .build();
+
+connection.start()
+    .then(() => console.log('Connected to SignalR hub'))
+    .catch(err => console.error('Error Connecting to hub', err));
+
+connection.on('ReceiveMessage', message => {
+    console.log('Received message:', message);
+})
+
+function App() {
+//Removed template starting page
+
+}
+
+export default App
---
+++ WebChat/Program.cs
@@ -0,0 +1,65 @@
+using WebChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddSignalR();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapHub<ChatHub>("/chatHub");
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
+
+/* 
+***********************************************************************************************
+This is the template for the webapi dotnet - leaving it here to reference
+***********************************************************************************************
+
+// Configure the HTTP request pipeline. 
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+} */
